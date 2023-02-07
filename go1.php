00e2a97f6a0500a5fb83d4a0a3b1dde1d233a5d4
Ronnie <123381621+cezary016@users.noreply.github.com>
2023-02-07 18:44:15 +0200

    Create go1.php

---
+++ go1.php
@@ -0,0 +1,49 @@
+equire (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.3.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4 v4.1.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2 v2.1.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.0.0
+	github.com/Masterminds/sprig/v3 v3.2.3
+	github.com/aws/aws-sdk-go-v2 v1.17.3
+	github.com/aws/aws-sdk-go-v2/config v1.18.8
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.80.0
+	github.com/civo/civogo v0.3.23
+	github.com/digitalocean/godo v1.93.0
+	github.com/dirien/ovh-go-sdk v0.2.0
+	github.com/exoscale/egoscale v0.88.0
+	github.com/fatih/color v1.14.0
+	github.com/google/uuid v1.3.0
+	github.com/gophercloud/gophercloud v1.1.1
+	github.com/hashicorp/go-cleanhttp v0.5.2
+	github.com/hashicorp/go-retryablehttp v0.7.2
+	github.com/hetznercloud/hcloud-go v1.39.0
+	github.com/ionos-cloud/sdk-go/v6 v6.1.3
+	github.com/linode/linodego v1.11.0
+	github.com/melbahja/goph v1.3.0
+	github.com/oracle/oci-go-sdk/v65 v65.28.1
+	github.com/packethost/packngo v0.29.0
+	github.com/pkg/errors v0.9.1
+	github.com/scaleway/scaleway-sdk-go v1.0.0-beta.12
+	github.com/sethvargo/go-password v0.2.0
+	github.com/stretchr/testify v1.8.1
+	github.com/vultr/govultr/v2 v2.17.2
+	go.uber.org/zap v1.24.0
+	golang.org/x/crypto v0.5.0
+	golang.org/x/oauth2 v0.4.0
+	google.golang.org/api v0.108.0
+)
+
+require (
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.1.2 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
